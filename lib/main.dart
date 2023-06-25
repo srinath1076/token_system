baa427c5f8e7e3869adc3353434880a78f6cab65
srinath Venkat <srinath.venkataramani@gmail.com>
2023-06-25 07:21:01 +0000

    Gitpod docker, yml and main dart sample draft first commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
-import 'dart:io';
+import 'package:flutter/material.dart';
 
 void main() {
-  print('Hello, world!');
-  stdout.writeln('This is a simple Dart program.');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My First Flutter App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First Flutter App'),
+        ),
+        body: Center(
+          child: Text('Hello, world!'),
+        ),
+      ),
+    );
+  }
 }
